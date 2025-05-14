6719f171223b99353516f95b1c13328749f0ef79
akshar51 <aksharparekh401@gmail.com>
2025-05-14 13:11:53 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,12 @@ const App = () => {
 
   const handleSubmit = (e)=>{
     e.preventDefault()
+    const listAdd = [...list,task]
+    setList(listAdd)
+    console.log(list)
   }
+
+ 
 
   return (
     <>
@@ -32,30 +37,40 @@ const App = () => {
                       value={task.tasks}
                       onChange={handleChange}/>
                     </div>
-                    <div className="button-sec d-flex justify-content-between gap-3">
-                      <button className='btn btn-warning text-white fw-semibold w-100 py-3'>HTML / CSS</button>
-                      <button className='btn btn-success text-white fw-semibold w-100 py-3'>BOOTSTRAP</button>
-                      <button className='btn btn-danger text-white fw-semibold w-100 py-3'>JAVASCRIPT</button>
-                      <button className='btn btn-primary text-white fw-semibold w-100 py-3'>REACT</button>
-                      <button className='btn btn-secondary text-white fw-semibold w-100 py-3'>NODE JS</button>
+                    <div className="spanBox d-flex justify-content-between gap-3">
+
+                      <span
+                      onClick={()=>handleChange({target :{ name:"category" ,value:'HTML/CSS'}})}
+                      className='btn btn-warning text-white fw-semibold w-100 py-3'>
+                        HTML / CSS
+                      </span>
+
+                      <span className='btn btn-success text-white fw-semibold w-100 py-3'>BOOTSTRAP</span>
+                      <span className='btn btn-danger text-white fw-semibold w-100 py-3'>JAVASCRIPT</span>
+                      <span className='btn btn-primary text-white fw-semibold w-100 py-3'>REACT</span>
+                      <span className='btn btn-secondary text-white fw-semibold w-100 py-3'>NODE JS</span>
                     </div>
-                    <button type="submit" className="btn btn-success mt-3 py-3 px-3 fw-bold">Add Task</button>
-               </form>   
+                    <button type="submit" className="btn btn-success mt-3 py-3 px-3 fw-bold">Add Task</button> 
+               </form>
             </div>
-            <div className="data-show">
-              <div className="border mt-5 px-3 py-4 d-flex rounded">
-                <div className="fs-5 fw-bold me-auto">
-                  <span className=''>Croma</span>
-                </div>
-                  <div className="data-button">
-                  <button type="button" className="btn btn-primary position-relative me-2">
-                    Profile
-                    <span className="position-absolute top-0 start-100 translate-middle bg-danger border border-light rounded-circle">
-                      <i className="bi bi-x p-1" />
-                    </span>
-                  </button>
-                </div>
-              </div>
+            <div className="data-show mt-5">
+              {list.map((val,idx)=>{
+                return(
+                   <div className="border mt-3 px-3 py-4 d-flex rounded" key={idx}>
+                      <div className="fs-5 fw-bold me-auto">
+                        <span className=''>{val.tasks}</span>
+                      </div>
+                      <div className="data-button">
+                      <button type="button" className="btn btn-primary position-relative me-2">
+                        Profile
+                        <span className="position-absolute top-0 start-100 translate-middle bg-danger border border-light rounded-circle">
+                          <i className="bi bi-x p-1" />
+                        </span>
+                      </button>
+                      </div>
+                    </div>
+                )
+              })}
             </div>
           </div>
         </div>
